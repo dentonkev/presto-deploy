2ff8238c003a737dcf8095446d8065af7352bc25
Annie <annie.thai24@gmail.com>
2026-04-06 22:36:41 +1000

    Implemented route and UI for presentation controls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Landing from "./pages/Landing.tsx";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
+import Presentation from "./pages/Presentation.tsx";
 import { ErrorProvider } from "./context/ErrorContext.tsx";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/presentation/:id" element={<Presentation />} />
         </Routes>
       </ErrorProvider>
     </BrowserRouter>
