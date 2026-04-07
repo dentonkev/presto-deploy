9eff7dae8af10c11133ded31356aab7fd4f85188
Annie <annie.thai24@gmail.com>
2026-04-07 17:03:14 +1000

    Added in basic UI for editing presentation title

--- frontend/src/pages/Presentation.tsx
+++ frontend/src/pages/Presentation.tsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom"
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
-import { FaArrowLeft, FaTrashAlt } from "react-icons/fa";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton } from "@mui/material";
+import { FaArrowLeft, FaTrashAlt, FaEdit } from "react-icons/fa";
 import { useState, Fragment, useEffect } from "react";
 import { MdDelete } from "react-icons/md";
 import { apiDeletePresentation, apiFetchStore } from "../api";
@@ -69,30 +69,40 @@ const Presentation = () => {
 
   return (
     <>
-      <section className="flex h-screen">
-        <div className="flex flex-col justify-between p-3.5 bg-black h-full border-r border-solid border-[#323232]">
-          <button onClick={() => navigate("/dashboard")} aria-label="Go to Dashboard" className="cursor-pointer">
-            <FaArrowLeft className="text-gray-400 hover:text-red-500"/>
-          </button>
-          <button
-            aria-label="Delete"
-            className="cursor-pointer"
-            onClick={(e) => {
-              e.currentTarget.blur();
-              setOpen(true);
-            }}
-          >
-            <FaTrashAlt className="text-gray-400 hover:text-red-500"/>
-          </button>
+      <section className="flex flex-col h-screen w-screen overflow-hidden">
+        <div className="flex w-full gap-1 bg-[#1a1a1c] p-3.5 border-b border-solid border-[#323232] items-center">
+          <h2 className="text-white font-semibold">Title</h2> 
+          <IconButton>
+            <FaEdit color="white" size={15}/>
+          </IconButton>
         </div>
-        <div className="h-full w-full flex items-center justify-center align-center bg-white">
-          {slides.length === 0 ? (
-            <p>No slides available</p>
-          ) : (
-            <div className="w-[800px] h-[450px] bg-white flex items-center justify-center border border-dotted border-gray-300">
-              <p>Slide: {slides[0].id}</p>
-            </div>
-          )}
+        <div className="flex h-full">
+          <div className="flex flex-col justify-between p-3.5 bg-black h-full border-r border-solid border-[#323232]">
+            <button onClick={() => navigate("/dashboard")} aria-label="Go to Dashboard" className="cursor-pointer">
+              <FaArrowLeft className="text-gray-400 hover:text-red-500"/>
+            </button>
+            <button
+              aria-label="Delete"
+              className="cursor-pointer"
+              onClick={(e) => {
+                e.currentTarget.blur();
+                setOpen(true);
+              }}
+            >
+              <FaTrashAlt className="text-gray-400 hover:text-red-500"/>
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center align-center bg-white">
+            {slides.length === 0 ? (
+              <p>No slides available</p>
+            ) : (
+              <div className="w-full max-w-5xl aspect-video bg-white flex items-center justify-center border border-dotted border-gray-300 m-3">
+                <p>Slide: {slides[0].id}</p>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col justify-between p-3.5 bg-black h-full border-r border-solid border-[#323232]">
+          </div>
         </div>
       </section>
       <DeleteDialog
