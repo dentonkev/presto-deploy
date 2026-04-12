595979e35845c6f34429f64ae6bb9e59756c59d8
Annie <annie.thai24@gmail.com>
2026-04-12 23:08:10 +1000

    Added code modal to presentation

--- frontend/src/pages/Presentations.tsx
+++ frontend/src/pages/Presentations.tsx
@@ -15,6 +15,7 @@ import { SideBar } from "../components/SideBar";
 import { ToolBar } from "../components/ToolBar";
 import { Settings } from "../components/Settings";
 import { RightSideBar } from "../components/RightSideBar";
+import { CodeModal } from "../components/CodeModal";
 
 export type SlideElement = {
   xSize: string;
@@ -25,6 +26,7 @@ export type SlideElement = {
   color?: string;
   alt?: string;
   autoplay?: boolean;
+  language?: "c" | "python" | "javascript";
 }
 
 export type SlideData = {
@@ -53,7 +55,7 @@ const Presentations = () => {
 
   // Text elements
   const [text, setText] = useState(false);
-  const [fontSize, setFontSize] = useState<string>("1.5"); // in em
+  const [fontSize, setFontSize] = useState<string>("1"); // in em
   const [color, setColor] = useState<string>("#000000");
 
   // Image elements
@@ -63,6 +65,9 @@ const Presentations = () => {
   // Video elements
   const [video, setVideo] = useState(false);
   const [autoplay, setAutoplay] = useState(false);
+
+  // Code elements
+  const [code, setCode] = useState(false);
 
   // Delete Dialog config
   const deleteDialogTitle = deleteMode === "presentation" ? "You are deleting the full presentation." : "This slide will be permanently removed.";
@@ -247,6 +252,8 @@ const Presentations = () => {
 
   useEffect(() => {
     const handleArrowKeyDown = (event: KeyboardEvent) => {
+      if (text || video || image || code) return;
+
       if (event.key === "ArrowRight" && slides.length > 1 && currentSlide !== (slides.length - 1)) {
         setCurrentSlide(currentSlide + 1)
       } else if (event.key === "ArrowLeft" && slides.length > 1 && currentSlide !== 0) {
@@ -257,7 +264,7 @@ const Presentations = () => {
     return () => {
       window.removeEventListener("keydown", handleArrowKeyDown);
     };
-  }, [currentSlide, slides.length]);
+  }, [currentSlide, slides.length, text, video, image, code]);
 
   return (
     <>
@@ -300,6 +307,7 @@ const Presentations = () => {
             setImage={setImage}
             setAutoplay={setAutoplay}
             setVideo={setVideo}
+            setCode={setCode}
             handleDeleteElement={handleDeleteElement}
           />
           {openTools && (
@@ -315,6 +323,7 @@ const Presentations = () => {
               setImage={setImage}
               setAutoplay={setAutoplay}
               setVideo={setVideo}
+              setCode={setCode}
             />
           )}
           {openSettings && (
@@ -423,6 +432,19 @@ const Presentations = () => {
         setContent={setContent}
         autoplay={autoplay}
         setAutoplay={setAutoplay}
+        handleCreateElement={handleCreateElement}
+      />
+      <CodeModal
+        code={code}
+        setCode={setCode}
+        xSize={xSize}
+        setXSize={setXSize}
+        ySize={ySize}
+        setYSize={setYSize}
+        content={content}
+        setContent={setContent}
+        fontSize={fontSize}
+        setFontSize={setFontSize}
         handleCreateElement={handleCreateElement}
       />
     </>
