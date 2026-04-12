05641ff3fde041959420a5163627e1f8206a836d
Denton Kev <dentonkev122@gmail.com>
2026-04-12 12:31:30 +1000

    Move text modal to own component

--- frontend/src/pages/Presentations.tsx
+++ frontend/src/pages/Presentations.tsx
@@ -8,6 +8,7 @@ import type { Presentation } from "./Dashboard";
 import ErrorContext from "../context/ErrorContext";
 import { v4 as uuidv4 } from "uuid";
 import { DeleteDialog } from "../components/DeleteModal";
+import { TextModal } from "../components/TextModal";
 
 type SlideElement = {
   xSize: string;
@@ -502,33 +503,21 @@ const Presentations = () => {
           </Button>
         </Box>
       </Modal>
-      <Modal onClose={() => setText(false)} open={text}>
-        <Box className="absolute flex flex-col top-1/2 left-1/2 w-fit -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-white p-6 shadow-xl gap-4">
-          <label className="text-gray-500 flex flex-col">
-            xSize (%)
-            <input type="number" min={0} max={100} className="text-black border border-[#cecece] rounded-sm p-2" value={xSize} onChange={(e) => setXSize(e.target.value)}></input>
-          </label>
-          <label className="text-gray-500 flex flex-col">
-            ySize (%)
-            <input type="number" min={0} max={100} className="text-black border border-[#cecece] rounded-sm p-2" value={ySize} onChange={(e) => setYSize(e.target.value)}></input>
-          </label>
-          <TextField label="Text" variant="outlined" value={content} onChange={(e) => setContent(e.target.value)}></TextField>
-          <TextField label="Font Size" type="number" variant="outlined" value={fontSize} onChange={(e) => setFontSize(e.target.value)}></TextField>
-          <label className="text-gray-500 flex flex-col">
-            Text Colour
-            <input type="color" value={color} onChange={(e) => setColor(e.target.value)}></input>
-          </label>
-          <Button 
-            size="small" 
-            className="self-end" 
-            onClick={() => {
-              handleCreateElement({xSize, ySize, content, fontSize, color, type: "text"}, setText);
-            }}
-          >
-            Add text
-          </Button>
-        </Box>
-      </Modal>
+      <TextModal
+        setText={setText}
+        text={text}
+        xSize={xSize}
+        setXSize={setXSize}
+        ySize={ySize}
+        setYSize={setYSize}
+        content={content}
+        setContent={setContent}
+        fontSize={fontSize}
+        setFontSize={setFontSize}
+        color={color}
+        setColor={setColor}
+        handleCreateElement={handleCreateElement}
+      />
       <Modal onClose={() => setVideo(false)} open={video}>
         <Box className="absolute flex flex-col top-1/2 left-1/2 w-fit -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-white p-6 shadow-xl gap-4">
           <label className="text-gray-500 flex flex-col">
